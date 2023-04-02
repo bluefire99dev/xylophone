0aaea17de98613f762c7aa745835986e2649a238
bluefire99dev <bluefire99dev@gmail.com>
2023-04-02 14:24:24 +0530

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,12 @@ void main() {
 class XyloPhone extends StatelessWidget {
   const XyloPhone({super.key});
 
+//function for sound in button
+  void playsound(int soundNumber) {
+    final playerAudio = AudioCache();
+    playerAudio.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,63 +28,61 @@ class XyloPhone extends StatelessWidget {
             children: [
               TextButton(
                 onPressed: () {
-                  final playerAudio = AudioCache();
-                  playerAudio.play('note1.wav');
+                  playsound(1);
                 },
                 style: TextButton.styleFrom(backgroundColor: Colors.amber),
                 child: Text("1"),
               ),
               TextButton(
                 onPressed: () {
-                  final playerAudio = AudioCache();
-                  playerAudio.play('note2.wav');
+                  playsound(2);
                 },
                 style: TextButton.styleFrom(
-                    backgroundColor: Color.fromARGB(255, 61, 255, 7)),
+                  backgroundColor: Color.fromARGB(255, 61, 255, 7),
+                ),
                 child: Text("2"),
               ),
               TextButton(
                 onPressed: () {
-                  final playerAudio = AudioCache();
-                  playerAudio.play('note3.wav');
+                  playsound(3);
                 },
                 style: TextButton.styleFrom(
-                    backgroundColor: Color.fromARGB(255, 77, 7, 255)),
+                  backgroundColor: Color.fromARGB(255, 77, 7, 255),
+                ),
                 child: Text("3"),
               ),
               TextButton(
                 onPressed: () {
-                  final player = AudioCache();
-                  player.play('note4.wav');
+                  playsound(4);
                 },
                 style: TextButton.styleFrom(
-                    backgroundColor: Color.fromARGB(255, 180, 72, 14)),
+                  backgroundColor: Color.fromARGB(255, 180, 72, 14),
+                ),
                 child: Text("4"),
               ),
               TextButton(
                 onPressed: () {
-                  final player = AudioCache();
-                  player.play('note5.wav');
+                  playsound(5);
                 },
                 style: TextButton.styleFrom(
-                    backgroundColor: Color.fromARGB(255, 55, 43, 9)),
+                  backgroundColor: Color.fromARGB(255, 55, 43, 9),
+                ),
                 child: Text("5"),
               ),
               TextButton(
                 onPressed: () {
-                  final player = AudioCache();
-                  player.play('note6.wav');
+                  playsound(6);
                 },
                 style: TextButton.styleFrom(backgroundColor: Colors.red),
                 child: Text("6"),
               ),
               TextButton(
                 onPressed: () {
-                  final player = AudioCache();
-                  player.play('note7.wav');
+                  playsound(7);
                 },
                 style: TextButton.styleFrom(
-                    backgroundColor: Color.fromRGBO(255, 0, 247, 1)),
+                  backgroundColor: Color.fromRGBO(255, 0, 247, 1),
+                ),
                 child: Text("7"),
               ),
             ],
